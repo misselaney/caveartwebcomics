2e6659bd80be5fc6a2b4f6609ed9dd073f2ee075
Marissa Conner <marissafromtexas@gmail.com>
2022-07-17 10:51:04 -0700

    front end post request set up to send a cute file

--- src/pages/UploadComic.tsx
+++ src/pages/UploadComic.tsx
@@ -1,27 +1,48 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 function UploadComic() {
 
   const [files, setFiles] = useState<FileList>()
   const [src, setSrc] = useState<string>()
+  const [error, setError] = useState<boolean>(false)
 
   const generatePreview = function(e: React.ChangeEvent<HTMLInputElement>) {
     const target = e.target
     if (target.files !== null) {
       setFiles(target.files)
-      console.dir(target.files)
       setSrc(URL.createObjectURL(target.files[0]))
     }
   }
 
   const uploadFile = function () {
     const formData = new FormData()
+    const input = document.querySelector('#comicpage') as HTMLInputElement
+    if (input.files) {
+      const upload = input.files[0]
+      formData.append('file', upload)
+      axios({
+        method: 'post',
+        url: '/api/comic/upload',
+        data: formData
+      })
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((err) => {
+          console.log(err)
+          setError(true)
+        })
+    }
+
+   
   }
 
   return (
     <div>
       <form>
         <input
+          id="comicpage"
           type="file"
           accept="image/png, image/gif, image/jpg, image/jpeg"
           onChange={(e) => generatePreview(e)}
@@ -36,9 +57,7 @@ function UploadComic() {
       <label htmlFor="upload_time">Time of day</label>
       <input type="time" id="upload_time" />
 
-
       <button type="button" onClick={() => {uploadFile()}}>Upload</button>
-
     </div>
   )
 }
