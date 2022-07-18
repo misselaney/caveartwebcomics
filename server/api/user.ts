7f0756b999d20f36f29c6d6377789b6e3befa3fe
Marissa Conner <marissafromtexas@gmail.com>
2022-07-17 20:04:09 -0700

    moving files around - dedicated user controller file + routes file

--- server/api/user.ts
+++ server/api/user.ts
@@ -1,61 +1,10 @@
-import  Express, { Request, Response } from 'express'
-import { QueryResult } from 'pg'
-import { createHash, compareHash, createRandom } from '../utils/hash'
-import { auth } from './database/auth'
-
+import Express from 'express'
+import userController from '../controllers/userController'
 const userRoutes = Express.Router()
 
-userRoutes.post('/new', async (req: Request, res: Response) => {
-  const email = req.body.email.replace(/[^a-zA-Z0-9@._-]/gi, '')
-  const hashedPassword = createHash(req.body.password)
-  const queryResult = await auth.createNewUser(email, hashedPassword)
-  if (queryResult.error) {
-    res.status(500).send(queryResult)
-  } else {
-    res.status(200).send(queryResult)
-  }
-})
-
-userRoutes.post('/login', async (req: Request, res: Response) => {
-  const email = req.body.email.replace(/[^a-zA-Z0-9@._-]/gi, '')
-  const hashedPassword = createHash(req.body.password)
-  const queryResult = await auth.getUserByCredentials(email, hashedPassword)
-  if (queryResult.error) {
-    res.status(500).send(queryResult)
-  } else {
-    const userId = queryResult.id
-    const sessionHash = createHash(createRandom())
-    auth.updateUserSession(userId, sessionHash)
-    res.cookie('caveartwebcomicssession', sessionHash)
-    res.cookie('caveartwebcomicsuser', userId)
-    const token = {
-      caveartwebcomics: sessionHash,
-      caveartwebcomicsId: userId
-    }
-    res.status(200).send(token)
-  }
-})
-
-userRoutes.post('/logout', async (req: Request, res: Response) => {
-  const id = req.cookies.caveartwebcomicsuser
-  const session = req.cookies.caveartwebcomicssession
-  const queryResult = await auth.clearUserSession(session, id)
-  if (queryResult.error) {
-    res.status(500).send(queryResult)
-  } else {
-    res.status(200).send()
-  }
-})
-
-userRoutes.get('/session', async (req: Request, res: Response) => {
-  const id = req.cookies.caveartwebcomicsuser
-  const session = req.cookies.caveartwebcomicssession
-  const queryResult = await auth.confirmUserSession(session, id)
-  if (queryResult.error) {
-    res.status(403).send(queryResult)
-  } else {
-    res.status(200).send(queryResult)
-  }
-})
+userRoutes.post('/new', userController.createUser)
+userRoutes.post('/login', userController.login)
+userRoutes.post('/logout', userController.logout)
+userRoutes.get('/session', userController.getSession)
 
 export default userRoutes
